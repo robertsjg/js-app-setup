014902738069770f5cbb16580817ca648bd7cae6
JohnGarethRoberts <gareth.spud@gmail.com>
2016-12-29 09:26:41 +0000

    removed ref to bundle.js in index.html

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
     debug: true,
@@ -15,6 +16,10 @@ export default {
         filename: 'bundle.js'
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: true
+        }),
         // Eliminate duplicate packages when generating bundle,
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin() // minify JS
