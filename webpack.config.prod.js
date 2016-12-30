7a6dd69e150becd4104f5ff5376ec2a5008aef99
JohnGarethRoberts <gareth.spud@gmail.com>
2016-12-30 09:03:17 +0000

    added bundler

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,21 +1,34 @@
 import path from 'path';
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import WebpackMd5Hash from 'webpack-md5-hash';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
 
 export default {
     debug: true,
     devtool: 'source-map',
     noInfo: false,
-    entry: [
-        path.resolve(__dirname, 'src/index')
-    ],
+    entry: {
+        vendor: path.resolve(__dirname, 'src/vendor'),
+        main: path.resolve(__dirname, 'src/index')
+    },
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: '[name].[chunkhash].js'
     },
     plugins: [
+        // generate a css file with hashed filename
+        new ExtractTextPlugin('[name].[conthash].css'),
+        // cache bust so file names change when their content changes
+        new WebpackMd5Hash(),
+        // name must match key in entry (vendor)
+        // bundle splitting
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             minify: {
@@ -39,7 +52,7 @@ export default {
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
-            { test: /\.css$/, loaders: ['style', 'css'] }
+            { test: /\.css$/, loader: ExtractTextPlugin.extract('css?sourceMap') }
         ]
     }
 }
