eb1086076250b1161de990eaa059b61fcad2f858
JohnGarethRoberts <gareth.spud@gmail.com>
2017-01-31 11:00:59 +0000

    added lg4js,cors and favicon

--- buildScripts/devServer.js
+++ buildScripts/devServer.js
@@ -3,7 +3,10 @@ import open from 'open';
 import webpackdevserver from 'webpack-dev-server';
 import config from '../webpack.config.dev';
 import morgan from 'morgan';
+import log4js from 'log4js';
 import fs from 'file-system';
+import cors from 'cors';
+import favicon from 'serve-favicon';
 
 /* eslint-disable no-console */
 
@@ -23,14 +26,41 @@ var webServerConfig = {
     }
 };
 const port = 3000;
-
 var app = new webpackdevserver(compiler, webServerConfig);
 
-var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' })
+app.use(cors());
 
-// setup the logger
-app.use(morgan('combined', { stream: accessLogStream }))
+// cors whitelist - useful if API is split from static SPA
+var whitelist = ['http://eg1.com', 'http://eg2.com'];
+var corsOptions = {
+    origin: function(origin, callback) {
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+    }
+};
 
+app.use(favicon(__dirname + '/favicon.png'));
+
+var log = log4js.getLogger('devServer');
+// setup the http logger (using Morgan)
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' });
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.use(function(req, res, next) {
+    var err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    log.Error('We have a problem', err);
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    });
+    next(err);
+});
 
 app.listen(port, function(err) {
     if (err) {
